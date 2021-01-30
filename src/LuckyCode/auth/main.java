e5ebf8cd377226e83f1a4f7e0ee037c33b750aed
iniga72 <49905453+iniga72@users.noreply.github.com>
2021-01-30 13:46:55 +0300

    ad

--- src/LuckyCode/auth/main.java
+++ src/LuckyCode/auth/main.java
@@ -11,33 +11,32 @@ import java.io.IOException;
 import java.util.HashMap;
 
 public class main extends JavaPlugin {
-    public static File fileconfig = new File(Bukkit.getPluginManager().getPlugin("LuckyAuth").getDataFolder() + File.separator + "config.yml");
-    public static File fileparam = new File(Bukkit.getPluginManager().getPlugin("LuckyAuth").getDataFolder() + File.separator + "params.yml");
     public void onEnable() {
-        if(!fileconfig.exists()) {
+        File file = new File(Bukkit.getPluginManager().getPlugin("LuckyAuth").getDataFolder() + File.separator + "config.yml");
+        if(!file.exists()) {
             getConfig().options().copyDefaults(true);
             saveDefaultConfig();
         }
-        config = YamlConfiguration.loadConfiguration(fileconfig);
-        fileparam = new File(getDataFolder()
+        config = YamlConfiguration.loadConfiguration(file);
+        file = new File(getDataFolder()
                 + File.separator + "params.yml");
-        if(!fileparam.exists()) {
+        if(!file.exists()) {
             try {
-                fileparam.createNewFile();
+                file.createNewFile();
             } catch (IOException ignored) {
             }
         }
         params = YamlConfiguration.loadConfiguration(new File(Bukkit.getPluginManager().getPlugin("LuckyAuth").getDataFolder() + File.separator + "params.yml"));
         Bukkit.getPluginManager().registerEvents(new events(), this);
         getCommand("register").setExecutor(new register());
-        getCommand("luckyauth").setExecutor(new reload());
+        getCommand("luckyauth").setExecutor(new register());
         getCommand("changepassword").setExecutor(new changepassword());
         getCommand("code").setExecutor(new code());
         getCommand("login").setExecutor(new login());
-/*
+
         for (Player p : Bukkit.getOnlinePlayers()) {
             authorization(p);
-        }*/
+        }
     }
     public static FileConfiguration config;
     public static FileConfiguration params;
