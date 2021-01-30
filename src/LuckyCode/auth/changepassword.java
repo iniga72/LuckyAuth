f5835ce4ac6ff9ff0e552cd7089b05fb0ef9f440
iniga72 <49905453+iniga72@users.noreply.github.com>
2021-01-30 11:15:03 +0300

    ad

--- src/LuckyCode/auth/changepassword.java
+++ src/LuckyCode/auth/changepassword.java
@@ -7,7 +7,7 @@ import org.bukkit.command.CommandSender;
 public class changepassword implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-        if()
+
             sender.sendMessage("Гыг");
         return true;
     }
--- src/LuckyCode/auth/login.java
+++ src/LuckyCode/auth/login.java
@@ -7,7 +7,7 @@ import org.bukkit.command.CommandSender;
 public class login  implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-        if()
+
             sender.sendMessage("Гыг");
         return true;
     }
