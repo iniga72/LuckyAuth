c65cbc4f70fd6a6063af40b70d97acf8d5bc5118
K1RYSHQBTW <77577454+K1RYSHQBTW@users.noreply.github.com>
2021-01-30 12:42:48 +0300

    вавававаапа

--- src/LuckyCode/auth/register.java
+++ src/LuckyCode/auth/register.java
@@ -3,12 +3,20 @@ package LuckyCode.auth;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class register implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if(!(sender instanceof Player)){
+            sender.sendMessage("Вы не являетесь игроком!");
+            return true;
+        }
+        if(main.isRegister(sender.getName())){
+           sender.sendMessage(main.config.getString("messages.isregister").replace("§", "&"));
+           return true;
+        }
 
-        sender.sendMessage("Гыг");
         return true;
     }
 }
